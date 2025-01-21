8752b29690438213fb3d9b68e669f51de9b451e7
unknown <sr0589071@gmail.com>
2025-01-21 23:55:16 +0600

    create an payment section

--- src/Layout/Payment/Payment.jsx
+++ src/Layout/Payment/Payment.jsx
@@ -1,23 +1,4 @@
-// import { Elements } from '@stripe/react-stripe-js';
-// import React from 'react';
-// import { loadStripe } from 'react-stripe-js';
-// import CheckoutForm from './CheckoutForm';
 
-// const Payment = () => {
-//     const stripePromise = loadStripe('import.meta.env.VITE_Payment_Gateway_PK');
-//     return (
-//         <div>
-//             <h2 className="text-4xl">Payment This</h2>
-//             <div>
-//                 <Elements stripe={stripePromise}>
-//                     <CheckoutForm></CheckoutForm>
-//                 </Elements>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Payment;
 
 import { Elements } from '@stripe/react-stripe-js';
 import React from 'react';
